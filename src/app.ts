0e50264783d2095b8245e33743c4758e70cb208e
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2026-03-08 14:43:15 -0500

    feat: added app.ts and error middleware into expres

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+    accessLogger,
+    errorLogger,
+    consoleLogger,
+} from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHandler";
+import healthRoutes from "./api/v1/routes/healthRoutes";
+import authRoutes from "./api/v1/routes/authRoutes";
+import loanRoutes from "./api/v1/routes/loanRoutes";
+import userRoutes from "./api/v1/routes/userRoutes";
+import adminRoutes from "./api/v1/routes/adminRoutes";
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+    app.use(accessLogger);
+    app.use(errorLogger);
+} else {
+    app.use(consoleLogger);
+}
+
+app.use(express.json());
+
+app.use("/api/v1/health", healthRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/loans", loanRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/admin", adminRoutes);
+
+app.use(errorHandler);
+
+export default app;
